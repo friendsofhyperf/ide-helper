fb6b425ac56a116e6c09884e14eb7f031895160a
huangdijia <huangdijia@gmail.com>
2021-06-25 17:20:00 +0800

    update php-cs-fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,5 +1,14 @@
 <?php
 
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://github.com/friendsofhyperf/ide-helper
+ * @document https://github.com/friendsofhyperf/ide-helper/blob/master/README.md
+ * @contact  huangdijia@gmail.com
+ * @license  https://github.com/friendsofhyperf/ide-helper/blob/master/LICENSE
+ */
 $header = <<<'EOF'
 This file is part of Hyperf.
 
@@ -9,7 +18,7 @@ This file is part of Hyperf.
 @license  https://github.com/friendsofhyperf/ide-helper/blob/master/LICENSE
 EOF;
 
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config())
     ->setRiskyAllowed(true)
     ->setRules([
         '@PSR2' => true,
@@ -17,19 +26,19 @@ return PhpCsFixer\Config::create()
         '@DoctrineAnnotation' => true,
         '@PhpCsFixer' => true,
         'header_comment' => [
-            'commentType' => 'PHPDoc',
+            'comment_type' => 'PHPDoc',
             'header' => $header,
             'separate' => 'none',
             'location' => 'after_declare_strict',
         ],
         'array_syntax' => [
-            'syntax' => 'short'
+            'syntax' => 'short',
         ],
         'list_syntax' => [
-            'syntax' => 'short'
+            'syntax' => 'short',
         ],
         'concat_space' => [
-            'spacing' => 'one'
+            'spacing' => 'one',
         ],
         'blank_line_before_statement' => [
             'statements' => [
@@ -38,7 +47,7 @@ return PhpCsFixer\Config::create()
         ],
         'general_phpdoc_annotation_remove' => [
             'annotations' => [
-                'author'
+                'author',
             ],
         ],
         'ordered_imports' => [
@@ -62,11 +71,13 @@ return PhpCsFixer\Config::create()
         'multiline_whitespace_before_semicolons' => [
             'strategy' => 'no_multi_line',
         ],
+        'constant_case' => [
+            'case' => 'lower',
+        ],
         'class_attributes_separation' => true,
         'combine_consecutive_unsets' => true,
         'declare_strict_types' => true,
         'linebreak_after_opening_tag' => true,
-        'lowercase_constants' => true,
         'lowercase_static_reference' => true,
         'no_useless_else' => true,
         'no_unused_imports' => true,
@@ -81,6 +92,9 @@ return PhpCsFixer\Config::create()
     ])
     ->setFinder(
         PhpCsFixer\Finder::create()
+            ->exclude('bin')
+            ->exclude('public')
+            ->exclude('runtime')
             ->exclude('vendor')
             ->in(__DIR__)
     )
